c012a112e03aa9dca977b6a57aac241ee4a162c6
Johnston <jcgh00@okdaily.com>
2019-04-10 19:07:32 -0700

    item scroll

--- ts/js/MsgBtl.js
+++ ts/js/MsgBtl.js
@@ -1,17 +1,17 @@
-//4-10-2019 jchoy v1.129 scrollBy when using aniMap
+//4-10-2019 jchoy v1.131 item scroll
 Msg5do = function(){
-  this.ver= "1.128";
+  this.ver= "1.131";
   this.max= 10;
   var $t=this, sto=new Sto().lo, fox=Msg5do.fox;
   $t.tuHost= "$r";  //$t.tag= "#default"
   $t.res= {a:[], f:function(){console.log('no res fcn')} };
   var meh= fox.meh;
-  var as=["tmp/m5do","m5tst_cfg","2687"];
-  var og= fox.abc( "tmp/m5do","","2687" );
+  var og= fox.abc( "tmp/m5do","","2687","msgbtl_head_cfg" );
   this.start= function(tid,fn){
     $t.res.f= fn;  $t.res.a=[];  $t.res.tid=tid;
+    //if (!($t.res.head=sto.getItem(og.d))) $t.res.head=og.c;
     fox.tstu( og.c, og.a, sto, $t.tuHost );
-    fox.ttry( function(){return (sto.getItem(as[0]))?1:0},
+    fox.ttry( function(){return (sto.getItem(og.a))?1:0},
       function(){$t.fetHd()}, 20,
       function(){$t.fetAb()} );
   }
@@ -20,13 +20,13 @@ Msg5do = function(){
     $t.res.f($t.res.a);
   }
   this.fetHd= function(){
-    console.log( 'startHd..', sto.getItem(as[0]) );
-    var id0= sto.getItem(as[0]);
+    console.log( 'startHd..', sto.getItem(og.a) );
+    var id0= sto.getItem(og.a);
     if ($t.res.tid==id0)
       return $t.res.f([]);
-    fox.tstu(id0,as[0],sto,this.tuHost);
+    fox.tstu(id0,og.a,sto,this.tuHost);
     if (id0) fox.ttry(
-      function(){return (sto.getItem(as[0]))?1:0},
+      function(){return (sto.getItem(og.a))?1:0},
       function(){$t.fetCh(id0)}, 20,
       function(){$t.fetAb(id0)} );
   }
@@ -35,24 +35,16 @@ Msg5do = function(){
     return (jp=="") || ($t.res.a.length>=$t.max);
   }
   this.fetCh= function(fetId){
-    var jo= JSON.parse(sto.getItem(as[0]));
+    var jo= JSON.parse(sto.getItem(og.a));
     jo.id= fetId;
     $t.res.a.unshift( jo );
     if (this.feTest(jo.prev))
          return $t.res.f($t.res.a);
-    fox.tstu(jo.prev,as[0],sto,this.tuHost);
+    fox.tstu(jo.prev,og.a,sto,this.tuHost);
     if (jo.prev) fox.ttry(
-      function(){return (sto.getItem(as[0]))?1:0},
+      function(){return (sto.getItem(og.a))?1:0},
       function(){$t.fetCh(jo.prev)}, 20,
       function(){$t.fetAb(jo.prev)} );
-  }
-  this.bldJo= function(meh, dat, id){
-    var res={};
-    for (var i=0; i<meh.length; i++)
-      if (i<dat.length)
-        res[meh[i][0]]= new window[meh[i][1]](dat[i]);
-    if (id) res.id= id;
-    return res;
   }
 }
 Msg5do.fox= {
@@ -181,7 +173,7 @@ Btem= function(sto){
     var sheet = D[as[2]]('style');
     var bdr="border: 1px solid gray; border-radius: 5px;";
     sheet[as[5]]= "."+as[4]+" {"+bdr+"margins:0 2 0 2;"
-      +"width:70%; margin-left:15%; padding-left:2px}\n"
+      +"width:70%; margin-left:15%; padding-left:2px; overflow:scroll}\n"
       +"."+as[6]+" {"+bdr+"background-color:#DDBBBB;"
       +"margins:0 2 0 2; width:8%; text-align:center;"
       +"margin-left:45%}";
